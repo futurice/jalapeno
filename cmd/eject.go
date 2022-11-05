d6eccf47c62b3b018c537d6d7e609662150e7b7e
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-11-05 11:21:21 +0200

    Add eject command
    Could have a better name but "eject" just nukes all jalapeno files from a project (currently by deleting the `.jalapeno` directory).

---
+++ cmd/eject.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/futurice/jalapeno/pkg/recipe"
+	"github.com/spf13/cobra"
+)
+
+var (
+	targetPath = ""
+)
+
+func newEjectCmd() *cobra.Command {
+	var ejectCmd = &cobra.Command{
+		Use:   "eject",
+		Short: "Remove all Jalapeno-specific files",
+		Long:  "Remove all the files and directories that are for Jalapeno internal use, and leave only the rendered project files.",
+		Args:  cobra.ExactArgs(0),
+		Run:   ejectFunc,
+	}
+
+	ejectCmd.Flags().StringVarP(&targetPath, "path", "p", ".", "Location of project to eject")
+
+	return ejectCmd
+}
+
+func ejectFunc(cmd *cobra.Command, args []string) {
+	if _, err := os.Stat(targetPath); os.IsNotExist(err) {
+		fmt.Println("Project path does not exist")
+		return
+	}
+
+	jalapenoPath := filepath.Join(targetPath, recipe.RenderedRecipeDirName)
+
+	if stat, err := os.Stat(jalapenoPath); os.IsNotExist(err) || !stat.IsDir() {
+		fmt.Printf("'%s' is not a Jalapeno project\n", targetPath)
+		return
+	}
+
+	err := os.RemoveAll(jalapenoPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("\nEjected successfully!")
+}
