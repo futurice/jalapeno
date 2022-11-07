c1ba136f8dbb1116fc04492ae61f2e1572954a48
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-11-07 18:21:47 +0200

    Implement saving recipe stack
    Each recipe gets a stack index starting from 1. Saving the same recipe again retains its stack position.

--- pkg/recipe/saver.go
+++ pkg/recipe/saver.go
@@ -1,6 +1,7 @@
 package recipe
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -14,9 +15,34 @@ func (re *Recipe) Save(dest string) error {
 		return err
 	}
 
-	err = os.WriteFile(filepath.Join(dest, RecipeFileName), out, 0644)
+	// find our stack order ()
+	matches, err := filepath.Glob(filepath.Join(dest, fmt.Sprintf("*-%s.yml", re.Name)))
 	if err != nil {
+		// The only case for this should be a malformed glob pattern
 		return err
+	}
+	if len(matches) > 1 {
+		return fmt.Errorf("this should never happen: more than one rendered %s recipe in %s", re.Name, dest)
+	}
+
+	if len(matches) == 0 {
+		// No matches -- this is the first time rendering this recipe in this directory.
+		// The index therefore needs to be the next highest integer, which is the length
+		// of the list of all rendered recipes, plus 1.
+		allRendered, err := filepath.Glob(filepath.Join(dest, "*-*.yml"))
+		if err != nil {
+			return err
+		}
+		err = os.WriteFile(filepath.Join(dest, fmt.Sprintf("%d-%s.yml", len(allRendered)+1, re.Name)), out, 0644)
+		if err != nil {
+			return err
+		}
+	} else {
+		// One match -- this is the case where we're overwriting an existing version.
+		err = os.WriteFile(matches[0], out, 0644)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
