1f48bf87a56d64acb6e4b7f30f2d6a9b95931a78
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-02-17 09:33:26 +0200

    wip state management terraform recipe

---
+++ examples/terraform-bootstrap/templates/terraform/state-storage.tf
@@ -0,0 +1,37 @@
+data "azurerm_resource_group" "main" {
+  name     = {{ quote .Variables.RESOURCE_GROUP_NAME }}
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "{{ (printf "tfs%.11s%.6s" (regexReplaceAll "[^a-z0-9]" (.Variables.SERVICE_NAME | lower) "") (randNumeric 6)) }}${terraform.workspace}"
+  resource_group_name      = data.azurerm_resource_group.main.name
+  location                 = data.azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+#
+# TODO: "anchor" / "id" in rendered recipe (generated when executed, somehow uniquely identifies recipe + project combo)
+# TODO: family of stableRandomX helper functions for sprig where they always give the same value for the same id, e.g. "gimme 6 random alphanumeric characters that don't change on recipe upgrade"
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+}
+
+resource "local_file" "backend_config" {
+  filename = "backend.tf"
+  content = <<-EOT
+terraform {
+	backend "azurerm" {
+		storage_account_name = "${azurerm_storage_account.tfstate.name}"
+		container_name = "${azurerm_storage_container.tfstate.name}"
+	}
+}
+EOT
+}
+
+output "tfstate_storage_account_name" {
+  value = azurerm_storage_account.tfstate.name
+}
