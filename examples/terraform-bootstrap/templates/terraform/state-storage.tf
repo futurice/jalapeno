c825dd5de0cbd4f9202f7b119ddd16b7fa35a003
Ilkka Poutanen <ilkka@ilkka.dev>
2023-03-21 14:24:48 +0200

    Use stableRandom in tfstate storage account name

--- examples/terraform-bootstrap/templates/terraform/state-storage.tf
+++ examples/terraform-bootstrap/templates/terraform/state-storage.tf
@@ -18,16 +18,13 @@ data "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "{{ (printf "tfs%.11s%.6s" (regexReplaceAll "[^a-z0-9]" (.Variables.SERVICE_NAME | lower) "") (sha1sum .Recipe.Anchor)) }}${terraform.workspace}"
+  name                     = "{{ (printf "tfs%.11s" (regexReplaceAll "[^a-z0-9]" (.Variables.SERVICE_NAME | lower) "")) }}{{ stableRandomAlphanumeric 6 .Recipe.Anchor }}${terraform.workspace}"
   resource_group_name      = data.azurerm_resource_group.main.name
   location                 = data.azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
 }
-
-#
-# TODO: family of stableRandomX helper functions for sprig where they always give the same value for the same id, e.g. "gimme 6 random alphanumeric characters that don't change on recipe upgrade"
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
