2398727006ebfb6b3a770ecf50f313859ec33a3c
Antti Kivimäki <antti.kivimaki@futurice.com>
2023-11-07 16:09:37 +0200

    Create dedicated Docker image for GH action

---
+++ cmd/action/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/futurice/jalapeno/internal/cli"
+	"github.com/gofrs/uuid"
+)
+
+var (
+	// https://goreleaser.com/cookbooks/using-main.version/
+	version string
+)
+
+func main() {
+	filename := os.Getenv("GITHUB_OUTPUT")
+	if filename == "" {
+		checkErr(errors.New("GITHUB_OUTPUT environment variable not set"))
+	}
+
+	output, err := os.OpenFile(filename, os.O_APPEND, 0644)
+	checkErr(err)
+
+	cmd := cli.NewRootCmd(version)
+	delimiter := uuid.Must(uuid.NewV4()).String()
+	fmt.Fprintf(output, "result<<%s\n", delimiter)
+
+	// TODO: Add result
+
+	fmt.Fprintf(output, "%s\n", delimiter)
+
+	err = cmd.Execute()
+	checkErr(err)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
+	}
+}
