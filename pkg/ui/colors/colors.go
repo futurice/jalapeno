5004486cdc0f483436058fba41caeae8d710fdbd
Katajisto <t@ktj.st>
2024-10-02 08:45:47 +0300

    chore: make end of file marker gray

--- pkg/ui/colors/colors.go
+++ pkg/ui/colors/colors.go
@@ -3,7 +3,7 @@ package colors
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	Red    = lipgloss.NewStyle().Foreground(lipgloss.Color("#EF4136"))
-	Green  = lipgloss.NewStyle().Foreground(lipgloss.Color("#26A568"))
-	Yellow = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFF00"))
+	Red   = lipgloss.NewStyle().Foreground(lipgloss.Color("#EF4136"))
+	Green = lipgloss.NewStyle().Foreground(lipgloss.Color("#26A568"))
+	Gray  = lipgloss.NewStyle().Foreground(lipgloss.Color("#858585"))
 )
